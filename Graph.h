58d126f3a5b4e3f1bc7ad658189ab42be50082bc
Bram <MetalHeel@aol.com>
2010-08-08 20:48:49 -0500

    Final add; we should be done...

--- Graph.h
+++ Graph.h
@@ -31,12 +31,12 @@ class Graph {
         // typedefs
         // --------
 
-        typedef unsigned long vertex_descriptor;          // fix!
-        typedef pair<vertex_descriptor, vertex_descriptor> edge_descriptor;            // fix!
+        typedef unsigned long vertex_descriptor;
+        typedef pair<vertex_descriptor, vertex_descriptor> edge_descriptor;
 
-        typedef const vertex_descriptor* vertex_iterator;           // fix!
-        typedef const edge_descriptor* edge_iterator;             // fix!
-        typedef const vertex_descriptor* adjacency_iterator;        // fix!
+        typedef const vertex_descriptor* vertex_iterator;
+        typedef const edge_descriptor* edge_iterator;
+        typedef const vertex_descriptor* adjacency_iterator;
 
         typedef size_t vertices_size_type;
         typedef size_t edges_size_type;
@@ -47,7 +47,9 @@ class Graph {
         // --------
 
         /**
-         * <your documentation>
+         * @param  vone vertex_descriptor which will become the source of this edge
+         * @param  vtwo vertex_descriptor which will become the target of this edge
+         * @return pair containing the edge descriptor and a boolean which is true if the edge was successfully created
          */
         friend pair<edge_descriptor, bool> add_edge (vertex_descriptor vone, vertex_descriptor vtwo, Graph& theGraph) {
             edge_descriptor thisEdge = make_pair(vone, vtwo);
@@ -65,7 +67,9 @@ class Graph {
         // ----------
 
         /**
-         * <your documentation>
+         * Adds a vertex to the passed in graph.
+         * @param theGraph
+         * @return a vertex_descriptor for the newly created vertex
          */
         friend vertex_descriptor add_vertex (Graph& theGraph) {
             theGraph.g.push_back(vector<vertex_descriptor>());
@@ -77,7 +81,9 @@ class Graph {
         // -----------------
 
         /**
-         * <your documentation>
+         * @param v vertex_descriptor for which the request for adjacent verticies corresponds to
+         * @param theGraph
+         * @return a pair containing begining and ending iterators to go through all the adjacent vertices to vertex_descriptor v
          */
         friend pair<adjacency_iterator, adjacency_iterator> adjacent_vertices (vertex_descriptor v, const Graph& theGraph) {
             adjacency_iterator b = &*theGraph.g[v].begin();
@@ -89,7 +95,9 @@ class Graph {
         // ----
 
         /**
-         * <your documentation>
+         * @param vone the source vertex_descriptor of this edge
+         * @param vtwo the target vertex_descriptor of this edge
+         * @return a pair containing the edge descriptor for this edge and boolean, which is true if it exists
          */
         friend pair<edge_descriptor, bool> edge (vertex_descriptor vone, vertex_descriptor vtwo, const Graph& theGraph) {
             edge_descriptor ed = make_pair(vone, vtwo);
@@ -105,7 +113,8 @@ class Graph {
         /**
          * O(1) in space
          * O(1) in time
-         * <your documentation>
+         * @param theGraph
+         * @return a pair containing beginning and end iterators whose range is all of the edges in theGraph
          */
         friend pair<edge_iterator, edge_iterator> edges (const Graph& theGraph) {
             return make_pair(&*theGraph.my_edges.begin(), &*theGraph.my_edges.end());}
@@ -115,7 +124,8 @@ class Graph {
         // ---------
 
         /**
-         * <your documentation>
+         * @param theGraph
+         * @return the number of edges in theGraph
          */
         friend edges_size_type num_edges (const Graph& theGraph) {
             return theGraph.my_edges.size();}
@@ -125,7 +135,8 @@ class Graph {
         // ------------
 
         /**
-         * <your documentation>
+         * @param theGraph
+         * @return the number of vertices in theGraph.
          */
         friend vertices_size_type num_vertices (const Graph& theGraph) {
             return theGraph.my_vertices.size();}
@@ -135,7 +146,9 @@ class Graph {
         // ------
 
         /**
-         * <your documentation>
+         * @param ed edge_descriptor for the edge whose source is requested
+         * @param theGraph
+         * @return a vertex_descriptor for the source of this edge
          */
         friend vertex_descriptor source (edge_descriptor ed, const Graph& theGraph) {
             return ed.first;}
@@ -145,7 +158,9 @@ class Graph {
         // ------
 
         /**
-         * <your documentation>
+         * @param ed edge_descriptor for the edge whose target is requested
+         * @param theGraph
+         * @return a vertex_descriptor for the target of this edge
          */
         friend vertex_descriptor target (edge_descriptor ed, const Graph& theGraph) {
             return ed.second;}
@@ -155,7 +170,9 @@ class Graph {
         // ------
 
         /**
-         * <your documentation>
+         * @param vindex the index
+         * @param theGraph
+         * @return the vertex descriptor being indexed
          */
         friend vertex_descriptor vertex (vertices_size_type vindex, const Graph& theGraph) {
             if(theGraph.my_vertices.size() == 0)
@@ -167,7 +184,8 @@ class Graph {
         // --------
 
         /**
-         * <your documentation>
+         * @param theGraph
+         * @return a pair containing beginning and end iterators whose range is all of the vertices in theGraph
          */
         friend pair<vertex_iterator, vertex_iterator> vertices (const Graph& theGraph) {
             return make_pair(&*theGraph.my_vertices.begin(), &*theGraph.my_vertices.end());}
@@ -177,16 +195,16 @@ class Graph {
         // data
         // ----
 
-        vector< vector<vertex_descriptor> > g;
-        vector<edge_descriptor> my_edges;
-        vector<vertex_descriptor> my_vertices;
+        vector< vector<vertex_descriptor> > g; //structure relating sources to their targets, and implicitly their edges
+        vector<edge_descriptor> my_edges;      //list of all of the edge descriptors
+        vector<vertex_descriptor> my_vertices; //list of all of the verticies
 
         // -----
         // valid
         // -----
 
         /**
-         * <your documentation>
+         * @return true if this is a valid graph
          */
         bool valid () const {
             if(my_edges.size() == 0 && my_vertices.size() == 0)
@@ -204,7 +222,7 @@ class Graph {
         // ------------
 
         /**
-         * <your documentation>
+         * Graph's default constructor.
          */
         Graph () {
             assert(valid());}
